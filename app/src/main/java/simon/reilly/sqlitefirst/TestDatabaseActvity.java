9420155eece46510a08f52a5d2216de288773c90
Simon I. Reilly <sir32181@gmail.com>
2016-04-14 14:59:43 +0100

    Initial commit

---
+++ app/src/main/java/simon/reilly/sqlitefirst/TestDatabaseActvity.java
@@ -0,0 +1,13 @@
+package simon.reilly.sqlitefirst;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestDatabaseActvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_database_actvity);
+    }
+}
